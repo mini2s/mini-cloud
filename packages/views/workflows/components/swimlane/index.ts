0e21e55bb2d3f3d60bfc0ecc14843d4fcd136d22
IronRookieCoder <kdemo648@gmail.com>
2026-06-23 13:10:16 +0800

    feat(workflows): add swimlane barrel export

---
+++ packages/views/workflows/components/swimlane/index.ts
@@ -0,0 +1,6 @@
+export { WorkflowSwimlanePage } from "./workflow-swimlane-page";
+export type { WorkflowSwimlanePageProps } from "./workflow-swimlane-page";
+export { SwimlaneCanvas } from "./swimlane-canvas";
+export type { SwimlaneCanvasProps } from "./swimlane-canvas";
+export { computeSwimlaneLayout, LANE_HEADER_HEIGHT, LANE_PADDING, LANE_HEIGHT, LANE_SPACING } from "./swimlane-layout";
+export type { SwimlaneLayoutResult, SwimlaneLane } from "./swimlane-layout";
